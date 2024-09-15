1ec82bc50a35d4bcfcc56b9542291f144a8ea7ae
Oleksandr Veryk <veryk.av@gmail.com>
2024-09-15 23:55:07 +0300

    Initial commit

---
+++ task_1.py
@@ -0,0 +1,14 @@
+#1. Дано 4 числа a, b, c, d. Знайти max{min(a,b),min(c,d) }.
+
+print("To find the larger number from two pairs of smaller numbers, enter four numbers")
+
+num_a = int(input("Enter the first number a: "))
+num_b = int(input("Enter the second number b: "))
+num_c = int(input("Enter the third number с: "))
+num_d = int(input("Enter the fourth number b: "))
+
+max_of_min = max(min(num_a,num_b), min(num_c,num_d ))
+
+print("There is a maximum of the minimum:", max_of_min)
+
+
